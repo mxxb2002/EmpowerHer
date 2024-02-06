da462fe9434ba1e0bd11fb8c57dbc1db2711b52b
mxxb2002 <124915567+mxxb2002@users.noreply.github.com>
2024-02-06 21:46:22 +0000

    Delete src directory

--- src/db.js
+++
@@ -1,7 +0,0 @@
-//Database connection
-const { MongoClient } = require('mongodb');
-
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-module.exports = client;
--- src/routes/registerRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerControllerMethod } = require('../controllers/registerController');
-
-router.post('/', registerControllerMethod);  // Use registerControllerMethod directly
-
-module.exports = router;
